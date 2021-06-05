704a986ce81533cea9506489fcaa8c7b6dc2d65b
Nithin47TA <nithinta234@gmail.com>
2021-06-05 22:34:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+var listitem = ["hello"];
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public/"));
+app.set('view engine', 'ejs');
+app.get("/", function(req, res) {
+
+  res.render('list', {
+    day: date.day(),
+    list: listitem
+  });
+});
+
+app.post("/", function(req, res) {
+  listitem.push(req.body.Item);
+  res.redirect("/");
+
+});
+
+app.listen(3000, function() {
+  console.log("connected to port 3000");
+});
