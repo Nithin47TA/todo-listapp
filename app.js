a78c60ca770043ad6ee1f7df8b83926bc2af415b
Nithin47TA <nithinta234@gmail.com>
2021-09-24 22:19:37 +0530

    port correction

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ app.get("/:newList",function(req,res){
 });
 
 
-let port=process.env.PORT||3000;
+let port=process.env.PORT||4000;
 app.listen(port, function() {
   console.log("connected to port : "+port);
 });
